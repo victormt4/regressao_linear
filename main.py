d460080f606a71242c4cbb554e70efea1081c446
Victor Mota <victor.vmt@outlook.com>
2020-06-05 17:02:13 -0300

    Melhorando boxplot

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+# setando estilo dos gráficos
+sns.set_palette('Accent')
+sns.set_style('darkgrid')
+
 dataset = pd.read_csv('data/Consumo_cerveja.csv', sep=';')
 
 print('\nMatriz de correlação:\n')
@@ -16,9 +20,12 @@ ax.set_xlabel('Dias', fontsize=16)
 
 plt.show()
 
-ax_boxplot = sns.boxplot(data=dataset['consumo'], orient='v', width=0.2)
+# Investigando variável dependente (y) segundo determinada característica
+# 0 = dia útil, 1 = fim de semana
+ax_boxplot = sns.boxplot(data=dataset, y='consumo', x='fds', orient='v', width=0.2)
 ax_boxplot.figure.set_size_inches(12, 6)
 ax_boxplot.set_title('Consumo de Cerveja', fontsize=14)
 ax_boxplot.set_ylabel('Litros', fontsize=12)
+ax_boxplot.set_xlabel('Final de Semana', fontsize=12)
 
 plt.show()
