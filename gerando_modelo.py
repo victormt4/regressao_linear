b6ceeabd27bed17d45786725e82e429d409aea15
Victor Mota <victor.vmt@outlook.com>
2020-06-12 09:30:41 -0300

    Adicionando explicação dos coeficientes de regressão

--- gerando_modelo.py
+++ gerando_modelo.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 import pandas as pd
+import numpy as np
 
 dataset = pd.read_csv('data/Consumo_cerveja.csv', sep=';')
 
@@ -38,4 +39,12 @@ temp_max = 40
 chuva = 0
 fds = 1
 X_input = [[temp_max, chuva, fds]]
-print(f'Consumo previsto: {model.predict(X_input)[0].round(2)} litros')
+print(f'Consumo previsto: {model.predict(X_input)[0].round(2)} litros\n')
+
+# Visualizando os coeficientes da regressão
+# Intercepto: Mantendo as variáveis explicativas = 0, o efeito médio no consumo de cerveja seria 5951 litros
+# X1, X2, X3: Mantendo os outros coeficientes constantes, o acréscimo de 1 unidade em Xi gera uma variação média
+# no consumo de cerveja de X1 = 684 litros, X2 = -60 litros, X3 = 5401 litros
+index = ['Intercepto', 'Temperatura Máxima', 'Chuva (mm)', 'Final de Semana']
+coeficientes = pd.DataFrame(data=np.append(model.intercept_, model.coef_), index=index, columns=['Parâmetros'])
+print(coeficientes)
