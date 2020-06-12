0fd996b3de325fc7b520807c55c64fadfb178472
Victor Mota <victor.vmt@outlook.com>
2020-06-12 09:57:51 -0300

    Adicionando gráficos de visualização do modelo

--- gerando_modelo.py
+++ gerando_modelo.py
@@ -3,6 +3,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('data/Consumo_cerveja.csv', sep=';')
 
@@ -48,3 +50,32 @@ print(f'Consumo previsto: {model.predict(X_input)[0].round(2)} litros\n')
 index = ['Intercepto', 'Temperatura Máxima', 'Chuva (mm)', 'Final de Semana']
 coeficientes = pd.DataFrame(data=np.append(model.intercept_, model.coef_), index=index, columns=['Parâmetros'])
 print(coeficientes)
+
+# Visualizando gráficos do modelo
+
+y_preview_train = model.predict(X_train)
+errors = y_train - y_preview_train
+
+ax = sns.scatterplot(x=y_preview_train, y=y_train)
+ax.set_title('Previsão x Real')
+ax.set_xlabel('Consumo de Cerveja (litros) - Previsão')
+ax.set_ylabel('Consumo de Cerveja (litros) - Real')
+plt.show()
+
+ax = sns.scatterplot(x=y_preview_train, y=errors, s=150)
+ax.set_title('Previsão Resíduos')
+ax.set_xlabel('Consumo de Cerveja (litros) - Previsão')
+ax.set_ylabel('Resíduos')
+plt.show()
+
+# Checando se a variância dos resíduos é constante
+ax = sns.scatterplot(x=y_preview_train, y=errors**2, s=150)
+ax.set_title('Previsão Resíduos²')
+ax.set_xlabel('Consumo de Cerveja (litros) - Previsão')
+ax.set_ylabel('Resíduos²')
+plt.show()
+
+ax = sns.distplot(errors)
+ax.set_title('Distribuição de Frequências dos Resíduos')
+ax.set_xlabel('Litros')
+plt.show()
