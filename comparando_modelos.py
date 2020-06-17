be2755909647501cbc337bffb94b42f96fb159ca
Victor Mota <victor.vmt@outlook.com>
2020-06-17 16:54:08 -0300

    Adicionando outras métricas

--- comparando_modelos.py
+++ comparando_modelos.py
@@ -17,9 +17,29 @@ y_preview = model.predict(X_test)
 y2_preview = model_2.predict(X2_test)
 
 # Obtendo R² da previsão
+R2 = metrics.r2_score(y_test, y_preview).round(2)
+R2_2 = metrics.r2_score(y2_test, y2_preview).round(2)
 print('R² da previsão:')
-print(f'R² Temp. Máxima = {metrics.r2_score(y_test, y_preview).round(2)}')
-print(f'R² Temp. Média = {metrics.r2_score(y2_test, y2_preview).round(2)}\n')
+print(f'R² Temp. Máxima = {R2}')
+print(f'R² Temp. Média = {R2_2}\n')
+
+# Utilizando outras métricas de comparação
+# EQM = Erro quadrático médio
+# REQM = Raiz quadrática média
+
+EQM = metrics.mean_squared_error(y_test, y_preview)
+REQM = np.sqrt(EQM).round(2)
+
+EQM_2 = metrics.mean_squared_error(y2_test, y2_preview)
+REQM_2 = np.sqrt(EQM_2).round(2)
+
+# R² - Quanto mais próximo de 1, melhor
+# EQM e REQM, quando menor, melhor
+metrics_temp_max = pd.DataFrame([EQM, REQM, R2], ['EQM', 'REQM', 'R²'], columns=['Métricas - Temp Máxima'])
+metrics_temp_media = pd.DataFrame([EQM_2, REQM_2, R2_2], ['EQM', 'REQM', 'R²'], columns=['Métricas - Temp Média'])
+
+print(pd.concat([metrics_temp_max, metrics_temp_media], axis=1))
+print('\n')
 
 # Visualizando os coeficientes da regressão
 # Intercepto: Mantendo as variáveis explicativas = 0, o efeito médio no consumo de cerveja seria 5951 litros
