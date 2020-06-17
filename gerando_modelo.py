c50043f59936840afa7ac71c359341414f196032
Victor Mota <victor.vmt@outlook.com>
2020-06-17 16:27:25 -0300

    Gerando outro modelo para comparação

--- gerando_modelo.py
+++ gerando_modelo.py
@@ -13,24 +13,33 @@ y = dataset['consumo']
 
 # Criando um DataFrame com as variáveis explicativas (X)
 X = dataset[['temp_max', 'chuva', 'fds']]
+X2 = dataset[['temp_media', 'chuva', 'fds']]
 
 # Criando os datasets de treino e teste
 # test size aloca 0.3% do dataset para teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2811)
+X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y, test_size=0.3, random_state=2811)
 
 model = LinearRegression()
 model.fit(X=X_train, y=y_train)
 
+model_2 = LinearRegression()
+model_2.fit(X=X2_train, y=y2_train)
+
 # Obtendo coeficiente de determinação (R²)
 # Medida resumida que diz o quanto a linha de regressão se ajusta aos dados. È um valor entre 0 e 1.
 # Quanto mais próximo de 1, melhor
-print(f'R² = {model.score(X_train, y_train).round(2)}')
+print(f'R² Temp. Máxima = {model.score(X_train, y_train).round(2)}')
+print(f'R² Temp. Média = {model_2.score(X2_train, y2_train).round(2)}\n')
 
 # Gerando previsões
 y_preview = model.predict(X_test)
+y2_preview = model_2.predict(X2_test)
 
 # Obtendo R² da previsão
-print(f'R² = {metrics.r2_score(y_test, y_preview).round(2)}\n')
+print('R² da previsão:')
+print(f'R² Temp. Máxima = {metrics.r2_score(y_test, y_preview).round(2)}')
+print(f'R² Temp. Média = {metrics.r2_score(y2_test, y2_preview).round(2)}\n')
 
 # Gerando previsão pontual
 X_input = X_test[0:1]
@@ -41,7 +50,7 @@ temp_max = 40
 chuva = 0
 fds = 1
 X_input = [[temp_max, chuva, fds]]
-print(f'Consumo previsto: {model.predict(X_input)[0].round(2)} litros\n')
+print(f'Consumo previsto simulação: {model.predict(X_input)[0].round(2)} litros\n')
 
 # Visualizando os coeficientes da regressão
 # Intercepto: Mantendo as variáveis explicativas = 0, o efeito médio no consumo de cerveja seria 5951 litros
